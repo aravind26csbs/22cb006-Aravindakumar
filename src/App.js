a75559d1d7c5d33b646b585a2277b3ce4b707a17
AravindaKumar K <152989083+aravind26csbs@users.noreply.github.com>
2024-11-08 14:19:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! - Aravindakumar K - 22CB006 
+          Hello World! - Aravindakumar K -- 22CB006 
         </p>
       </header>
     </div>
